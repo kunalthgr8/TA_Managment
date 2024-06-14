d692f159d499f20c974ab0a4540255346cec8357
kunalthgr8 <kunalsingla@iitbhilai.ac.in>
2024-06-14 19:07:53 +0530

    Basic frontend Setup

---
+++ Frontend/TA_MANAGEMENT/src/components/index.js
@@ -0,0 +1,27 @@
+import Button from "./Button/Button";
+import ChangePassword from "./ChangePassword/ChangePassword";
+import Dashboard from "./Dashboard/Dashboard";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import Input from "./Input/Input";
+import Leaves from "./Leaves/Leaves";
+import Loader from "./Loader/Loader";
+import Login from "./Login/Login";
+import Logo from "./Logo/Logo";
+import Searchbar from "./Searchbar/Searchbar";
+import Signup from "./Signup/signup";
+
+export {
+  Button,
+  ChangePassword,
+  Dashboard,
+  Header,
+  Home,
+  Input,
+  Leaves,
+  Loader,
+  Login,
+  Logo,
+  Searchbar,
+  Signup,
+};
