c079087212aa4d2f308c8dd745b9e2f3a5b6b7d1
Lalit Gour <lalitg@iitbhilai.ac.in>
2024-07-18 19:06:44 +0530

    academics resolver and typeDefs added for ml model

---
+++ Backend/src/resolvers/faculty/academics.resolver.js
@@ -0,0 +1,13 @@
+// import {ApiError} from '../../utils/ApiError.js';
+// import {ApiResponse} from '../../utils/ApiResponse.js';
+
+// const academicsResolvers = {
+//   Query: {
+//     
+//   },
+//   Mutation: {
+//     
+//   }
+// };
+
+// export default academicsResolvers;
